540fdd21987154411c0afe98e3233399865c2445
Andrew Ho <helloworld922@gmail.com>
2026-05-28 18:10:46 -0700

    added cmake option for CAMP_USE_PLATFORM_DEFAULT_STREAM

--- include/camp/config.in.hpp
+++ include/camp/config.in.hpp
@@ -15,6 +15,7 @@
 #cmakedefine CAMP_ENABLE_HIP
 #cmakedefine CAMP_ENABLE_SYCL
 #cmakedefine CAMP_WIN_STATIC_BUILD
+#cmakedefine CAMP_USE_PLATFORM_DEFAULT_STREAM
 #endif
 
 #define CAMP_VERSION_MAJOR @camp_VERSION_MAJOR@
